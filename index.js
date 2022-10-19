ecad9ced3bd0364e52f22036b87087c074624ea5
Kyle Holzinger <kylelholzinger@gmail.com>
2022-10-19 08:15:38 -1000

    [KH] add more chaos

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 var red = document.querySelector('.crt')
+var counter = new Date()
 
 function setProperty(duration) {
   red.style.setProperty('--animation-time', duration + 's')
 }
 
 function changeAnimationTime() {
-  var animationDuration = Math.random() + 1.5
+  var animationDuration =
+    (Math.random() + 1.5) / Math.max(((new Date() - counter) / 10000) * 2, 1)
   setProperty(animationDuration)
 }
 
